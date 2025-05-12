cfe3c97efb85b17e12773a00d61f9d8f8b7138aa
AbbyAdj <abbyadjei9@gmail.com>
2025-05-13 00:04:36 +0100

    write pg8000 connection object

---
+++ src/utils/connection.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from pg8000.native import Connection
+
+load_dotenv(override=True)
+
+
+def connect_to_db():
+    PG_USER = os.getenv("DB_USER", default="postgres")
+    PG_HOST = os.getenv("DB_HOST", default="localhost")
+    PG_DATABASE = os.getenv("DB_DATABASE", default="postgres")
+    PG_PASSWORD = os.getenv("DB_PASSWORD")
+
+    # print(PG_USER, "host:", PG_HOST, PG_DATABASE, PG_PASSWORD)    
+    conn = Connection(
+        user=PG_USER, host=PG_HOST, database=PG_DATABASE, password=PG_PASSWORD
+    )
+
+    return conn
+
+
+def close_db_connection(conn):
+    conn.close()
