506a0c46ffa543091e94fe7582aef7da6ef89235
AbbyAdj <abbyadjei9@gmail.com>
2025-05-14 16:39:20 +0100

    add new user  and get user albums endpoint

--- src/api/main.py
+++ src/api/main.py
@@ -2,7 +2,7 @@ from pprint import pprint
 from fastapi import FastAPI, HTTPException, Request, UploadFile, File, Form
 from pg8000.dbapi import DatabaseError
 from src.utils.db_operations import util_funcs
-from src.models.models import PostPictureModel
+from src.models.models import PostPictureModel, AddNewUserModel
 # from src.api.exception_handler import database_error_handler
 from src.utils.aws_utils import insert_into_bucket, delete_object_from_bucket, delete_album_from_bucket
 
@@ -43,6 +43,12 @@ def get_user_details(user_id: int):
     return response
 
 
+@app.get("/users/{user_id}/albums")
+def get_user_albums(user_id: int):
+    response = util_funcs["user_albums"](user_id=user_id)
+    return response
+
+
 @app.get("/users/{user_id}/albums/{album_id}")
 def get_user_album(user_id: int, album_id: int):
     response = util_funcs["user_album_details"](user_id=user_id, album_id=album_id)
@@ -59,6 +65,13 @@ def get_all_user_pictures(user_id: int):
 # POST REQUESTS
 ###########################
 
+@app.post("/users/add", status_code=201)
+def add_new_user(new_user: AddNewUserModel):
+    
+    new_user_response = util_funcs["add_new_user"](user_details=new_user)
+
+    return new_user_response
+    
 
 @app.post("/users/{user_id}/albums/{album_id}/pictures", status_code=201)
 def post_new_picture(user_id: int, 
@@ -78,7 +91,6 @@ def post_new_picture(user_id: int,
     )
     return response
 
-# TODO ADD NEW USER (CREATES  DEFAULT ALBUM/BUCKET KEY)
 
 ###########################
 # PATCH REQUESTS
@@ -115,3 +127,5 @@ def delete_user_album(user_id: int, album_id: int):
                                                album_id=album_id,
                                                delete_confirmation = s3_delete_response)
     return response
+
+# TODO DELETE USER IS A RECURSIVE ACTION. DELETES EVERYTHING!
