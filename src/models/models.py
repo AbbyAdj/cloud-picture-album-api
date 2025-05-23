e4d321716a819052fd7eb8cf174980f337e1fbe5
AbbyAdj <abbyadjei9@gmail.com>
2025-05-23 18:40:00 +0100

    modify models

--- src/models/models.py
+++ src/models/models.py
@@ -2,10 +2,14 @@ from pydantic import BaseModel
 
 
 class PostPictureModel(BaseModel):
+    """Only use hyphens for the picture name and album name in place of spaces"""
     picture_name: str
-    picture_description: str
-    album_name: str
+    picture_description: str | None
 
 class AddNewUserModel(BaseModel):
     first_name: str
     last_name: str
+
+
+
+
