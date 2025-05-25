d800e437dd72fb7462b300a781b5bf986fb60729
AbbyAdj <abbyadjei9@gmail.com>
2025-05-25 14:57:38 +0100

    add exceptions

--- src/api/exception_handler.py
+++ src/api/exception_handler.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 from pg8000.dbapi import DatabaseError
-from src.api.main import app
+from botocore.exceptions import ClientError
 
 
-@app.exception_handler(DatabaseError)
 def database_error_handler(request: Request, exc: DatabaseError):
-    raise HTTPException(status_code=500, detail="Unknown Database Error")
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "A database error occurred."},
+    )
+
+def return_404_error(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+
+def aws_client_error(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "AWS Client Error Occured."},
+    )
+
+
+
